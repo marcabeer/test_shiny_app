7f3b03a669de91067e480dda6cee466dc207b3da
Marc Beer <marcabeer@yahoo.com>
2024-11-13 17:00:50 -0800

    Update test_shiny.R

--- test_shiny.R
+++ test_shiny.R
@@ -5,5 +5,6 @@ runApp("app1")
 install.packages(c("shinylive", "httpuv"))
 
 # shinylive code
+## tutorial for deploying app: https://medium.com/@rami.krispin/deploy-shiny-app-on-github-pages-b4cbd433bdc
 shinylive::export(appdir = "./app1", destdir = "./docs")
 httpuv::runStaticServer("docs/", port=8008)
